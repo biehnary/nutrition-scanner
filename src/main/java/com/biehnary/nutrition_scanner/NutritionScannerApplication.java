a0778f6f58ce7df3a7c95bae2fd47b858a01b26c
SooBiehn Hah <biehn.hah@gmail.com>
2025-06-03 15:32:24 +0900

    first commit
    this is first commit for my new proj

---
+++ src/main/java/com/biehnary/nutrition_scanner/NutritionScannerApplication.java
@@ -0,0 +1,13 @@
+package com.biehnary.nutrition_scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biehnary/nutrition_scanner/NutritionScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biehnary.nutrition_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
