b313530b1fab4c6434d769b05dd4c089dc966191
SooBiehn Hah <biehn.hah@gmail.com>
2025-06-08 10:07:42 +0900

    add API logic

--- src/main/java/com/biehnary/nutrition_scanner/service/FoodSearchService.java
+++ src/main/java/com/biehnary/nutrition_scanner/service/FoodSearchService.java
@@ -1,10 +1,46 @@
 package com.biehnary.nutrition_scanner.service;
 
 
+import com.biehnary.nutrition_scanner.product.FoodItem;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FoodSearchService {
+
+    @Value("${api.food.serviceKey}")
+    private String serviceKey;
+
+    private final WebClient webClient;
+    public Optional<FoodItem> searchItem(String foodNm) {
+        System.out.println("검색요청: " + foodNm);
+
+        try {
+            String response = webClient
+                    .get()
+                    .uri(uriBuilder -> uriBuilder
+                            .scheme("https")
+                            .host("apis.data.go.kr")
+                            .path("/1471000/FoodNtrCpntDbInfo02/getFoodNtrCpntDbInq02")
+                            .queryParam("serviceKey", serviceKey)
+                            .queryParam("FOOD_NM_KR", foodNm)
+                            .queryParam("pageNo", 1)
+                            .queryParam("numOfRows", 3)
+                            .build())
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            System.out.println("API 응답 : " + response);
+            return Optional.empty();
+        } catch (Exception e) {
+            System.out.println("API 호출 실패" + e.getMessage());
+            return Optional.empty();
+        }
+    }
 }
--- src/main/java/com/biehnary/nutrition_scanner/web/NutritionScannerController.java
+++ src/main/java/com/biehnary/nutrition_scanner/web/NutritionScannerController.java
@@ -1,19 +1,29 @@
 package com.biehnary.nutrition_scanner.web;
 
+import com.biehnary.nutrition_scanner.product.FoodItem;
+import com.biehnary.nutrition_scanner.service.FoodSearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 @RequiredArgsConstructor
 public class NutritionScannerController {
 
+    private final FoodSearchService foodSearchService;
+
     @GetMapping("/search/name")
     public String getByItemName(@RequestParam("foodNm") String foodNm, Model model) {
         System.out.println("제품명 받음: " + foodNm);
+
+        Optional<FoodItem> foodItem = foodSearchService.searchItem(foodNm);
+
         model.addAttribute("foodNm", foodNm);
+        model.addAttribute("foodItem", foodItem.orElse(null));
         return "search-result";
 
     }
